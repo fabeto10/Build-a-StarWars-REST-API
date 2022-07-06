f907a91522946dd72bcb5cc28585677ceebdba5e
fabeto10 <f0414475@gmail.com>
2022-07-06 21:09:54 +0000

    arreglando rutas

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-@app.route('/people', methods=["GET"])
+@app.route('/users/people', methods=["GET"])
 def list_people():
     people = Character.query.all()
     people_dictionary = list(map(
@@ -37,12 +37,12 @@ def list_people():
     ))
     return jsonify(people_dictionary)
 
-@app.route('/people/<int:people_id>', methods=["GET"])
+@app.route('/users/people/<int:people_id>', methods=["GET"])
 def list_single_people(people_id):
     character = Character.query.filter_by(id=people_id).one_or_none()
     return jsonify(character.serialize())
 
-@app.route('/planet', methods=['GET'])
+@app.route('/users/planet', methods=['GET'])
 def list_planets():
     planets = Planet.query.all()
     planets_dictionary = list(map(
@@ -51,42 +51,38 @@ def list_planets():
     ))
     return jsonify(planets_dictionary)
 
-@app.route('/planet/<int:planet_id>', methods=["GET"])
+@app.route('/users/planet/<int:planet_id>', methods=["GET"])
 def list_single_planet(planet_id):
-    planet = Planet.query.filter_by(id=planet_id).one_or_none()
+    planet = Planet.query.filter_by(planet_id).one_or_none()
     return jsonify(planet.serialize())
 
 @app.route('/users', methods=['GET'])
 def list_user_blog(user_id):
     users = User.query.all()
-    user_dictionary = list()(map(lambda user: user.serialize(), users))
+    user_dictionary = list((map(lambda user: user.serialize(), users)))
     return jsonify(user_dictionary)
 
 @app.route('/users/favorites', methods=['GET'])
 def list_favorite_user_planet(planet_id):
-    user_favorite_list_planet = User.query.filter_by(id=planet_id).one_or_none()
-    return serialize(user_favorite_list_planet)
+    user_favorite_list_planet = User.query.filter_by(planet_id).one_or_none()
+    return serialize(user_favorite_list_planet), 200
 def list_favorite_user_people(people_id):
-    user_favorite_list_people = User.query.filter_by(id=people_id).one_or_none()
-    return serialize(user_favorite_list_people)
+    user_favorite_list_people = User.query.filter_by(people_id).one_or_none()
+    return serialize(user_favorite_list_people), 200
 
-@app.route('/favorites/planet/<int:planet_id>', methods=['POST'])
-def add_favorite_planet():
+@app.route('/users/favorites/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
     global favorite_list
-    request_body = json.loads(request.data)
-    favorite_list.append(request_body)
-    print("in coming request with the favorite list", request_body)
+    favorite_list.append(planet_id)
     return jsonify(favorite_list), 200
 
-@app.route('/favorites/people/<int:people_id>', methods=['POST'])
-def add_favorite_people():
+@app.route('/users/favorites/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id):
     global favorite_list
-    request_body = json.loads(request.data)
-    favorite_list.append(request_body)
-    print("in coming request for the favorite list", request_body)
+    favorite_list.append(people_id)
     return jsonify(favorite_list), 200
 
-@app.route('/favorites/people/<int:people_id>', methods=['DELETE'])
+@app.route('/users/favorites/people/<int:people_id>', methods=['DELETE'])
 def delete_favorite_people(people_id):
     aux = []
     global favorite_list
@@ -94,9 +90,9 @@ def delete_favorite_people(people_id):
         if people_id != i:
             aux.append(favorite_list[i])
     favorite_list = aux
-    return jsonify(favorite_list)
+    return jsonify(favorite_list), 200
 
-@app.route('/favorites/planet/<int:planet_id>', methods=['DELETE'])
+@app.route('/users/favorites/planet/<int:planet_id>', methods=['DELETE'])
 def delete_favorite_planet(planet_id):
     aux = []
     global favorite_list
@@ -104,7 +100,7 @@ def delete_favorite_planet(planet_id):
         if planet_id != i:
             aux.append(favorite_list[i])
     favorite_list = aux
-    return jsonify(favorite_list)
+    return jsonify(favorite_list), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
